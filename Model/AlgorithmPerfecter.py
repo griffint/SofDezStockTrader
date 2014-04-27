df5b76c19fec75738eadec9986005092c8b641cd
Maor Bernstein <maor.bernstein@students.olin.edu>
2014-04-27 18:56:14 -0400

    Fixing things

---
+++ Model/AlgorithmPerfecter.py
@@ -0,0 +1,40 @@
+import numpy as np
+import scipy.linalg as linalg
+import matplotlib.pyplot as plt
+import dataFetcher as df
+def Convert(Stocks,Exog1,Exog2,Exog3):
+	""" This function converts data from EXOG and stocks lists to the A and b parameters for Ax=b"""
+	A = np.array([Exog1,Exog2,Exog3]).T
+	b = np.array(Stocks).T
+	return (A,b)
+
+def sumsqrs(l):
+	out = 0
+	for i in range(len(l)):
+		out += l[i]**2
+	return out
+
+def ParFinder(par):
+	dic = df.dataFetcher('AAPL')
+	Stocks = dic['ClosingPrices']
+	x1 = dic['DailyVolumes']
+	x2 = dic['ShortInterests']
+	x3 = dic['EarningsPerShare']
+	(A,b) = Convert(Stocks[:900],x1[:900],x2[:900],x3[:900])
+	(A_full,b_full) = Convert(Stocks,x1,x2,x3)
+	ATB = np.dot(A.transpose(),b)
+	numvars = ATB.shape[0]
+	ATA = np.dot(A.transpose(),A) + par*np.identity(numvars)
+	coeff = np.dot(linalg.inv(ATA),ATB)
+	predicted = np.dot(A_full,coeff)
+	errors = predicted - b_full
+	return sumsqrs(errors)
+
+if __name__ == "__main__":
+	pars = range(-0,100000,100 0)
+	out = []
+	for i in range(len(pars)):
+		out.append(ParFinder(pars[i]))
+	print out
+	plt.plot(pars,out)
+	plt.show()
