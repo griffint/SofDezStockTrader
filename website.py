abb1b54f022bc2abab531fa3c5b2a6f4db7710d9
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2014-05-04 18:58:33 -0400

    Analyzing

--- website.py
+++ website.py
@@ -10,6 +10,7 @@ from pattern.en import *
 from Model.symbolToName import get_company_name
 from Model.sentimentAnalysis import savefig_twitter_average
 from flask import Flask, render_template, request, redirect
+from Model.Analyze import Analyze
 #from Model.stocks import db
 
 app = Flask(__name__)
@@ -26,9 +27,18 @@ def hello_world():
 def search():
     search = request.form['searchkey']
     try:
+        print 1
         company_name = get_company_name(search)
+        print 2
         search=search.upper()
+        print 1
+    except:
+        pass
+    print Analyze(search)
+    try:
+        print 1
         savefig_twitter_average(company_name)
+        print 1
     except:
         company_name = 'AAAAAAAAAAAAAAAAA'
         #return redirect('/error')
@@ -45,5 +55,6 @@ def error():
     return render_template('error.html')
     
 if __name__ == "__main__":
+    print Analyze('WMT')
     app.run()
     
