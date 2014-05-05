73a4a94560f6bd1a8e72df502293c1e07c5b7d16
griffint <griffin.tschurwald@students.olin.edu>
2014-05-04 23:17:38 -0400

    I hav eno idea snymore

--- Model/stocks.py
+++ Model/stocks.py
@@ -16,10 +16,10 @@ import os
 #stuff to set up the database in Flask
 app = Flask(__name__)
 #config file tells where postgres URL is
-if "heroku" not in os.environ.get('HOME'):
+if "heroku" not in os.environ.get('WNHOME'):
 	app.config.from_pyfile('config.py')
 else:
-	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_IVORY_URL']
+	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 db = SQLAlchemy(app)
 
