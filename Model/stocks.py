61b74eb369ffece0e05fd0b9e8af559ecca092d3
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2014-05-04 21:32:25 -0400

    prints removed

--- Model/stocks.py
+++ Model/stocks.py
@@ -16,12 +16,9 @@ import os
 #stuff to set up the database in Flask
 app = Flask(__name__)
 #config file tells where postgres URL is
-print os.environ
 if "heroku" not in os.environ.get('WNHOME'):
-	print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! not in heroku !!!!!!!!!!!!!!!!!!!!!!!!"
 	app.config.from_pyfile('config.py')
 else:
-	print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! IN heroku !!!!!!!!!!!!!!!!!!!!!!!!"
 	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 db = SQLAlchemy(app)
