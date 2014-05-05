f7b5b634905d90940111fbc67bbc4fcf10de77d9
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2014-05-04 21:11:26 -0400

    please work for the love of humanity

--- Model/stocks.py
+++ Model/stocks.py
@@ -15,7 +15,11 @@ from sqlalchemy.ext.declarative import declarative_base
 #stuff to set up the database in Flask
 app = Flask(__name__)
 #config file tells where postgres URL is
-app.config.from_pyfile('config.py')
+if os.environ.get('HEROKU') is None:
+	app.config.from_pyfile('config.py')
+else:
+	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
 db = SQLAlchemy(app)
 
 #this class describes what each row of the table will be
